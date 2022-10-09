142bba29c8579534387ad59e5cc6f2495eea3b3f
Tapoban Ray <raytapoban@gmail.com>
2022-10-09 10:57:48 +0530

    Edited all Toplevel widgets
    Added title and geometry to all the Toplevel widgets

--- Contacts Manager.py
+++ Contacts Manager.py
@@ -8,32 +8,38 @@ def fill_details():
     top=Toplevel(bg='#F0DC82')
     top.geometry('700x400')
     top.title('Contact Manager')
-    mylabel = Label(top,text='Contact Manager',font=('Segoe UI Black',30),bg='#F0DC82').place(x=180,y=10)
+    Label(top,text='Contact Manager',font=('Segoe UI Black',30),bg='#F0DC82').place(x=180,y=10)
 
-    myButton = Button(top, text="Add Contact",font=('Malgun Gothic Bold',18),padx=52,pady=5,borderwidth=5,command=add_rec)
-    myButton.place(x=20,y=100)
+    Button(top, text="Add Contact",font=('Malgun Gothic Bold',18),padx=52,pady=5,borderwidth=5,command=add_rec).place(x=20,y=100)
 
-    myButton1 = Button(top, text="Delete Contact",font=('Malgun Gothic Bold',18),padx=40,pady=5,borderwidth=5,command=delete_rec) 
-    myButton1.place(x=20,y=200)
+    Button(top, text="Delete Contact",font=('Malgun Gothic Bold',18),padx=40,pady=5,borderwidth=5,command=delete_rec).place(x=20,y=200)
 
-    myButton2 = Button(top, text="Edit Contact",font=('Malgun Gothic Bold',18),padx=56,pady=5,borderwidth=5,command=edit_rec).place(x=400,y=100)
+    Button(top, text="Edit Contact",font=('Malgun Gothic Bold',18),padx=56,pady=5,borderwidth=5,command=edit_rec).place(x=400,y=100)
 
-    myButton3 = Button(top, text="View all Contacts",font=('Malgun Gothic Bold',18),padx=28,pady=5,borderwidth=5, command=view_all).place(x=400,y=200)
+    Button(top, text="View all Contacts",font=('Malgun Gothic Bold',18),padx=28,pady=5,borderwidth=5, command=view_all).place(x=400,y=200)
 
     Button(top,text='Exit',font=('Malgun Gothic Bold',18),padx=296,pady=5,borderwidth=5,command=root.quit).place(x=20,y=300)
 
 
 def  add_rec():
     top_add = Toplevel()
+    top_add.title('Add a Contact')
+    top_add.geometry('500x400')
 
 def delete_rec():
     top_delete = Toplevel()
+    top_delete.title('Delete a Contact')
+    top_delete.geometry('500x400')
 
 def edit_rec():
     top_edit = Toplevel()
+    top_edit.title('Edit a Contact')
+    top_edit.geometry('500x400')
 
 def view_all():
     top_view_all = Toplevel()
+    top_view_all.title('All Contacts')
+    top_view_all.geometry('500x400')
 
 
 text1 = "Contact Manager \n\n\n\n\n\n\nClick Anywhere to Continue"
